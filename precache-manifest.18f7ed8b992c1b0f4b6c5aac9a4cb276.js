9b966605259f45d19040eeb479edd7071f78b1ae
Ankit Upadhyay <56730118+ankitupadhyay889@users.noreply.github.com>
2020-08-07 18:13:51 +0530

    Updates

---
+++ precache-manifest.18f7ed8b992c1b0f4b6c5aac9a4cb276.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d08e94dc1aeb506a13b12471cfa902",
+    "url": "/livesearchapp/index.html"
+  },
+  {
+    "revision": "777794a6d7c98e298961",
+    "url": "/livesearchapp/static/js/2.8a00892e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/livesearchapp/static/js/2.8a00892e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b29c5d7367c40f89606f",
+    "url": "/livesearchapp/static/js/main.4471f7e3.chunk.js"
+  },
+  {
+    "revision": "f2ee6b3e58a0b675271d",
+    "url": "/livesearchapp/static/js/runtime-main.7fee7d47.js"
+  }
+]);
